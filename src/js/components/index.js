06b8fdc8e096acfaf0b5bd2e8fdb1fda91f68d9b
Валерий <kaktuz34@gmail.com>
2019-03-14 15:11:12 +0200

    startted cost  block

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,7 +1,4 @@
 import './_tabs';
 import './_slider';
 // import './_header';
-// import './_fancybox';
-// import './_validation';
-// import './_modal';
-// import './_dropdown';
+import './_validation';
